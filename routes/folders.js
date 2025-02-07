dc71426bede212d36e3de27f4986a3710aa258ac
Csb-218 <96022278+Csb-218@users.noreply.github.com>
2025-02-08 02:56:44 +0530

    feat : added file routes

--- routes/folders.js
+++ routes/folders.js
@@ -1,16 +1,42 @@
 var express = require('express');
 var router = express.Router();
-var {createFolder,updateFolder,deleteFolder} = require("../controllers/folderController")
+var {createFolder,updateFolder,deleteFolder,getAllFolders} = require("../controllers/folderController")
+var {upload} = require("../middlewares/fileupload");
+var {uploadFileToCloud,updateFileDescription,deleteFile, getAllFilesByFolder} = require("../controllers/fileController")
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+
+// Folder routes
+
+router.get('/', getAllFolders);
 
 router.post('/create', createFolder)
 
 router.put('/update/:folderId', updateFolder)
 
 router.delete('/delete/:folderId', deleteFolder)
+
+
+
+// File routes
+
+router.post('/:folderId/files',function(req,res,next){
+  upload(req,res,function(err){
+    if(err){
+      console.log(err)
+      return res.status(400).json({error:err.message})
+    }
+    next()
+  })
+},uploadFileToCloud)
+
+router.put('/:folderId/files/:fileId',updateFileDescription)
+
+router.delete('/:folderId/files/:fileId',deleteFile)
+
+router.get('/:folderId/files',getAllFilesByFolder)
+
+
+
+
 
 module.exports = router;
